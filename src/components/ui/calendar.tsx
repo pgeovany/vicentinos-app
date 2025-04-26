4941d3a14e755c890c8696c509536b79770d1af4
Pablo Souza <pgeovany14@gmail.com>
2025-04-26 05:53:10 -0300

    fix: conserta calendário

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,62 +3,20 @@
 import * as React from 'react';
 import { DayPicker } from 'react-day-picker';
 import { ptBR } from 'date-fns/locale';
-
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
+import 'react-day-picker/dist/style.css';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  locale = ptBR,
-  ...props
-}: CalendarProps) {
+function Calendar({ ...props }: CalendarProps) {
   return (
     <DayPicker
-      showOutsideDays={showOutsideDays}
-      locale={locale}
-      className={cn('p-3', className)}
+      style={{ '--rdp-accent-color': '#000' } as React.CSSProperties}
+      locale={ptBR}
+      className="rounded-lg shadow border overflow-hidden"
       classNames={{
-        months: 'flex flex-col sm:flex-row gap-2',
-        month: 'space-y-4',
-        caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
-        nav: 'space-x-1 flex items-center',
-        nav_button: cn(
-          buttonVariants({ variant: 'outline' }),
-          'size-7 bg-transparent p-0 opacity-50 hover:opacity-100',
-        ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        // table: 'w-full border-collapse',
-        table: 'w-full table-fixed border-collapse',
-
-        head_row: 'grid grid-cols-7',
-        row: 'grid grid-cols-7 mt-1',
-        head_cell:
-          'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex items-center justify-center',
-        // row: 'flex w-full justify-between mt-1',
-        cell: cn(
-          'relative p-0 text-center text-sm focus-within:relative focus-within:z-20 w-9 h-9 flex items-center justify-center',
-          props.mode === 'range'
-            ? '[&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md'
-            : '[&:has([aria-selected])]:bg-accent [&:has([aria-selected])]:rounded-md',
-        ),
-        day: cn(
-          buttonVariants({ variant: 'ghost' }),
-          'h-8 w-8 p-0 font-normal aria-selected:opacity-100',
-        ),
-        day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside: 'text-muted-foreground opacity-50',
-        day_disabled: 'text-muted-foreground opacity-50',
-        day_range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
-        day_hidden: 'invisible',
-        ...classNames,
+        // caption: 'bg-gray-50 py-2 rounded-t text-center',
+        caption: 'flex items-center justify-center gap-2 py-3 px-4 bg-gray-50 rounded-t',
+        caption_label: 'font-semibold text-md gap-2 py-2 px-3',
       }}
       {...props}
     />
